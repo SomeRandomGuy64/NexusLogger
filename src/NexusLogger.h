0b786bb11b30802c5ef3eb60c88791b44e3b043a
SomeRandomGuy64 <58070884+SomeRandomGuy64@users.noreply.github.com>
2025-06-30 18:12:05 +0100

    main features done

--- src/NexusLogger.h
+++ src/NexusLogger.h
@@ -1,1 +1,77 @@
 #pragma once
+
+#include <fmt/core.h>
+#include <fmt/chrono.h>
+#include <fmt/color.h>
+#include <chrono>
+#include <string>
+
+namespace Nexus {
+
+	class Logger {
+
+	public:
+
+		enum Color {
+
+			White = fmt::color::white,
+			Red = fmt::color::red,
+			Blue = fmt::color::aqua,
+			Green = fmt::color::green,
+			Yellow = fmt::color::yellow,
+			Orange = fmt::color::orange,
+			Purple = fmt::color::purple,
+			Pink = fmt::color::deep_pink,
+
+		};
+
+		enum Emphasis {
+
+			Blink = fmt::emphasis::blink,
+			Bold = fmt::emphasis::bold,
+			Conceal = fmt::emphasis::conceal,
+			Faint = fmt::emphasis::faint,
+			Italic = fmt::emphasis::italic,
+			Reverse = fmt::emphasis::reverse,
+			Strikethrough = fmt::emphasis::strikethrough,
+			Underline = fmt::emphasis::underline,
+
+		};
+
+		void log(const std::string& message, bool logTime = false, const Color color = Color::White, const Emphasis emphasis = Emphasis::Bold) {
+
+			const std::string timeNow{ getTime() };
+
+			fmt::color fmtColor{ static_cast<fmt::color>(color) };
+			fmt::emphasis fmtEmphasis{ static_cast<fmt::emphasis>(emphasis) };
+
+			switch (logTime) {
+
+				case false:
+					fmt::print(fmtEmphasis | fg(fmtColor), "{}\n", message);
+					break;
+
+				case true:
+					fmt::print(fmtEmphasis | fg(fmtColor), "[{}] {}\n", timeNow, message);
+					break;
+			}
+
+		}
+
+	private:
+
+		const std::string getTime() {
+
+			std::chrono::system_clock::time_point now{ std::chrono::system_clock::now() };
+			std::time_t timeNow{ std::chrono::system_clock::to_time_t(now) };
+			std::tm localTime{ *std::localtime(&timeNow) };
+
+			std::string formattedTime{ fmt::format("{:%H:%M:%S}", localTime) };
+
+			return formattedTime;
+
+		}
+
+	};
+
+}
--- src/example.cpp
+++ src/example.cpp
@@ -1,5 +1,22 @@
 #include "NexusLogger.h"
 
 int main() {
-	return 0;
+
+	Nexus::Logger logger;
+
+	logger.log("Hello from NexusLogger!");
+	logger.log("Hello from NexusLogger! (with time)", true);
+
+	//colour and emphasis output
+
+	logger.log("\n");
+	logger.log("White, default (bold)", true, logger.White);
+	logger.log("Green, blink", true, logger.Green, logger.Blink);
+	logger.log("Blue, conceal", true, logger.Blue, logger.Conceal);
+	logger.log("Yellow, faint", true, logger.Yellow, logger.Faint);
+	logger.log("Orange, italic", true, logger.Orange, logger.Italic);
+	logger.log("Red, reverse", true, logger.Red, logger.Reverse);
+	logger.log("Purple, strikethrough", true, logger.Purple, logger.Strikethrough);
+	logger.log("Pink, underline", true, logger.Pink, logger.Underline);
+
 }
